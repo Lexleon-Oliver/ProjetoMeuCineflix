8488113d7748d1cbc631e1317263f2c9a5dd8a52
Lexleon-Oliver <lexleonmg@gmail.com>
2022-01-22 13:19:36 -0300

    add rating movie

--- src/main/resources/static/js/filme.js
+++ src/main/resources/static/js/filme.js
@@ -1,10 +1,42 @@
 
 function getMovie(id){
-    request = new XMLHttpRequest()
+    let request = new XMLHttpRequest()
     request.open("GET","http://localhost:8080/api/v1/movies/"+id,false)
     request.send()
-    data = request.responseText
+    let data = request.responseText
     return JSON.parse(data);
+}
+
+function putMovie(movie){
+    const xhr = new XMLHttpRequest();
+
+// configure a `POST` request
+    xhr.open('PUT', 'http://localhost:8080/api/v1/movies/'+movie.id);
+
+// create a JSON object
+    const params = {
+        id: movie.id,
+        name: movie.name,
+        nameBr: movie.nameBr,
+        year: movie.year,
+        description: movie.description,
+        thumbnail: movie.thumbnail,
+        background: movie.background,
+        storage: movie.storage,
+        genre: movie.genre,
+        rating: movie.rating
+    };
+
+// set `Content-Type` header
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+// pass `params` to `send()` method
+    xhr.send(JSON.stringify(params));
+
+// listen for `load` event
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+    }
 }
 
 function assistir(){
@@ -15,25 +47,46 @@ function assistir(){
      localStorage.clear();
 }
 
+function avaliar(){
+    if(document.querySelector("#cm_star-5").checked){
+        selectedMovie.rating = 5
+    }else if(document.querySelector("#cm_star-4").checked){
+        selectedMovie.rating = 4
+    }else if(document.querySelector("#cm_star-3").checked){
+        selectedMovie.rating = 3
+    }else if(document.querySelector("#cm_star-2").checked){
+        selectedMovie.rating = 2
+    }else if(document.querySelector("#cm_star-1").checked){
+        selectedMovie.rating = 1
+    }else{
+        selectedMovie.rating = 0
+    }
+    putMovie(selectedMovie);
+
+}
+
 function getMovieData(){
-    movie = localStorage.getItem("movieId");
-    selectedMovie = getMovie(movie);
-    var nome = document.querySelector(".titulo-filme");
+    let movie = localStorage.getItem("movieId");
+    let selectedMovie = getMovie(movie);
+    let nome = document.querySelector(".titulo-filme");
     nome.textContent =selectedMovie.nameBr;
-    var sinopse = document.querySelector(".descricao-filme");
+    let sinopse = document.querySelector(".descricao-filme");
     sinopse.textContent = selectedMovie.description;
-    var capa = document.querySelector(".filme-selecionado");
+    let capa = document.querySelector(".filme-selecionado");
     capa.style.backgroundImage="url("+selectedMovie.background+")";
-    var nomeUs = document.querySelector(".nome-us-filme");
+    let nomeUs = document.querySelector(".nome-us-filme");
     nomeUs.textContent = "Nome inglês: "+selectedMovie.name;
-    var ano = document.querySelector(".ano-filme");
+    let ano = document.querySelector(".ano-filme");
     ano.textContent = "Ano: "+selectedMovie.year;
-    var genero = document.querySelector(".genero-filme");
+    let genero = document.querySelector(".genero-filme");
     genero.textContent = "Gênero: "+selectedMovie.genre;
-    var nota = document.querySelector(".nota-filme");
-    nota.textContent = "Minha nota: "+selectedMovie.rating;
+    let nota = document.querySelector(".nota-filme");
+    nota.textContent = "Minha nota: ";
+    let note = "#cm_star-" + selectedMovie.rating;
+    document.querySelector(note).checked =true;
 
 }
 
 getMovieData();
+
 
